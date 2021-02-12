a0fc67914cc555ec5cf7419c0ce994dcb7643d88
Dmitry Ovchinikov <thevitamine0@gmail.com>
2021-02-12 17:23:55 +0200

    further updates to the download / install component, permissions activity.

--- APK-Installer/src/main/java/com/classy/apk_installer/APKInstall.java
+++ APK-Installer/src/main/java/com/classy/apk_installer/APKInstall.java
@@ -10,6 +10,7 @@ import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.core.content.FileProvider;
@@ -19,23 +20,28 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Arrays;
 
 public class APKInstall extends AsyncTask<String, Integer, Boolean> {
 
     private ProgressBar bar;
+    private TextView text;
     private final Context context;
     private final String url_string;
     private Activity activity;
 
-    public APKInstall(Context context, Activity activity, ProgressBar bar, String url_string) {
+    public APKInstall(Context context, Activity activity, ProgressBar bar, TextView textView, String url_string) {
         this.context = context;
         this.activity = activity;
         this.bar = bar;
+        this.text = textView;
         this.url_string = url_string;
+        bar.setVisibility(View.VISIBLE);
     }
 
     @Override
     protected Boolean doInBackground(String... strings) {
+        Log.d("AAAT", "doInBackGround: ");
         boolean flag = false;
         try {
             URL url = new URL(url_string);
@@ -46,13 +52,14 @@ public class APKInstall extends AsyncTask<String, Integer, Boolean> {
             String PATH = Environment.getExternalStorageDirectory() + "/Download/";
             File file = new File(PATH);
             file.mkdirs();
-            File outputFile = new File(file, "ApkName.apk");
+            File outputFile = new File(file, "app.apk");
             if (outputFile.exists()) {
                 outputFile.delete();
             }
             FileOutputStream fos = new FileOutputStream(outputFile);
             InputStream is = c.getInputStream();
             int total_size = 4581692;
+            //int total_size = is.available();
             byte[] buffer = new byte[1024];
             int len1 = 0;
             int per = 0;
@@ -65,7 +72,7 @@ public class APKInstall extends AsyncTask<String, Integer, Boolean> {
             }
             fos.close();
             is.close();
-            OpenNewVersion(PATH);
+            installAPK(PATH);
             flag = true;
         } catch (Exception e) {
             Log.e("AAAT", "Update Error: " + e.getMessage());
@@ -75,28 +82,29 @@ public class APKInstall extends AsyncTask<String, Integer, Boolean> {
         return flag;
     }
 
-    void OpenNewVersion(String location) {
-        Uri uri = FileProvider.getUriForFile(activity, BuildConfig.LIBRARY_PACKAGE_NAME + ".provider", new File(location + "ApkName.apk"));
+    void installAPK(String location) {
+        Log.d("AAAT", "installAPK: ");
+        Uri uri = FileProvider.getUriForFile(activity, BuildConfig.LIBRARY_PACKAGE_NAME + ".fileprovider", new File(location + "ApkName.apk"));
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setDataAndType(uri, "application/vnd.android.package-archive");
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         context.startActivity(intent);
     }
 
     @Override
     protected void onPreExecute() {
+        Log.d("AAAT", "onPreExecute: ");
         super.onPreExecute();
         bar = new ProgressBar(activity);
-        //bar.setCancelable(false);
-        //bar.setMessage("Downloading...");
+        text.setText("Downloading...");
         bar.setIndeterminate(true);
-        //bar.setCanceledOnTouchOutside(false);
-        //bar.show();
         bar.setVisibility(View.VISIBLE);
     }
 
     protected void onProgressUpdate(Integer... progress) {
+        Log.d("AAAT", "onProgressUpdate: ");
         super.onProgressUpdate(progress);
         bar.setIndeterminate(false);
         bar.setMax(100);
@@ -107,14 +115,14 @@ public class APKInstall extends AsyncTask<String, Integer, Boolean> {
         } else {
             msg = "Downloading... " + progress[0] + "%";
         }
-        //bar.setMessage(msg);
+        text.setText(msg);
     }
 
     @Override
     protected void onPostExecute(Boolean result) {
-        // TODO Auto-generated method stub
+        Log.d("AAAT", "onPostExecute: ");
         super.onPostExecute(result);
-        bar.setVisibility(View.INVISIBLE);
+        activity.runOnUiThread(() -> bar.setVisibility(View.INVISIBLE));
         Log.d("AAAT", "Done I guess... Sadge");
         if (result) {
             Toast.makeText(activity.getApplicationContext(), "Done!!", Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/classy/apk_installer_example/MainActivity.java
+++ app/src/main/java/com/classy/apk_installer_example/MainActivity.java
@@ -4,19 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.classy.apk_installer.APKInstall;
 
 public class MainActivity extends AppCompatActivity {
 
     private ProgressBar progressBar;
+    private TextView example_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        example_text = findViewById(R.id.example_text);
         progressBar = findViewById(R.id.example_progress_bar);
-        new APKInstall(this, this, progressBar, "https://play.google.com/store/apps/details?id=com.netflix.mediaclient").execute();
+        //new APKInstall(this, this, progressBar, example_text, "https://play.google.com/store/apps/details?id=com.netflix.mediaclient").execute();
+        new APKInstall(this, this, progressBar, example_text, "https://www.getjar.com/download/984552/281107/?t=1613142062").execute();
     }
 }
